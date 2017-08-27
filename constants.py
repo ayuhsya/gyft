b3be7d35afc9653e6905ed083f6549cc71b35d99
ayuhsya <ayushm.kgp@gmail.com>
2017-08-28 06:38:23 +0900

    script now does not crash when there are no classes on a day

---
+++ constants.py
@@ -0,0 +1,11 @@
+ERP_HOMEPAGE_URL='https://erp.iitkgp.ernet.in/IIT_ERP3/'
+ERP_LOGIN_URL='https://erp.iitkgp.ernet.in/SSOAdministration/auth.htm'
+ERP_SECRET_QUESTION_URL='https://erp.iitkgp.ernet.in/SSOAdministration/getSecurityQues.htm'
+ERP_TIMETABLE_URL='https://erp.iitkgp.ernet.in/Acad/student/view_stud_time_table.jsp'
+
+headers = {
+    'timeout': 20,
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/51.0.2704.79 Chrome/51.0.2704.79 Safari/537.36',
+}
+
+days = ["", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
--- gyft.py
+++ gyft.py
@@ -5,34 +5,26 @@ from bs4 import BeautifulSoup as bs
 import re
 import json
 import getpass
+import constants
 
-#### Parsing from commmand line
+
+# Parsing from commmand line
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("-u", "--user", help="ERP Username/Login ID")
 args = parser.parse_args()
 erp_password = getpass.getpass("Enter your ERP password: ")
-
-#### Parsing ends
-
-ERP_HOMEPAGE_URL = 'https://erp.iitkgp.ernet.in/IIT_ERP3/'
-ERP_LOGIN_URL = 'https://erp.iitkgp.ernet.in/SSOAdministration/auth.htm'
-ERP_SECRET_QUESTION_URL = 'https://erp.iitkgp.ernet.in/SSOAdministration/getSecurityQues.htm'
+# Parsing ends
 
 
-
-headers = {
-    'timeout': 20,
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/51.0.2704.79 Chrome/51.0.2704.79 Safari/537.36',
-}
-
-s = requests.Session()
-r = s.get(ERP_HOMEPAGE_URL)
-soup = bs(r.text, 'html.parser')
+session = requests.Session()
+res = session.get(constants.ERP_HOMEPAGE_URL)
+soup = bs(res.text, 'html.parser')
 sessionToken = soup.find_all(id='sessionToken')[0].attrs['value']
-r = s.post(ERP_SECRET_QUESTION_URL, data={'user_id': args.user},
-           headers = headers)
-secret_question = r.text
+
+res = session.post(constants.ERP_SECRET_QUESTION_URL, data={'user_id': args.user},
+           headers = constants.headers)
+secret_question = res.text
 print (secret_question)
 secret_answer = getpass.getpass("Enter the answer to the security question: ")
 login_details = {
@@ -42,12 +34,10 @@ login_details = {
     'sessionToken': sessionToken,
     'requestedUrl': 'https://erp.iitkgp.ernet.in/IIT_ERP3',
 }
-r = s.post(ERP_LOGIN_URL, data=login_details,
-           headers = headers)
+res = session.post(constants.ERP_LOGIN_URL, data=login_details,
+           headers = constants.headers)
 ssoToken = re.search(r'\?ssoToken=(.+)$',
-                     r.history[1].headers['Location']).group(1)
-
-ERP_TIMETABLE_URL = "https://erp.iitkgp.ernet.in/Acad/student/view_stud_time_table.jsp"
+                     res.history[1].headers['Location']).group(1)
 
 timetable_details = {
     'ssoToken': ssoToken,
@@ -56,50 +46,50 @@ timetable_details = {
 }
 
 # This is just a hack to get cookies. TODO: do the standard thing here
-abc = s.post('https://erp.iitkgp.ernet.in/Acad/student/view_stud_time_table.jsp', headers=headers, data=timetable_details)
+abc = session.post('https://erp.iitkgp.ernet.in/Acad/student/view_stud_time_table.jsp', headers=constants.headers, data=timetable_details)
 cookie_val = None
-for a in s.cookies:
-    if (a.path == "/Acad/"):
-        cookie_val = a.value
+for entry in session.cookies:
+    if (entry.path == "/Acad/"):
+        cookie_val = entry.value
 
 cookie = {
     'JSESSIONID': cookie_val,
 }
-r = s.post('https://erp.iitkgp.ernet.in/Acad/student/view_stud_time_table.jsp',cookies = cookie, headers=headers, data=timetable_details)
+res = session.post('https://erp.iitkgp.ernet.in/Acad/student/view_stud_time_table.jsp',cookies = cookie, headers=constants.headers, data=timetable_details)
 
-soup = bs(r.text, 'html.parser')
+soup = bs(res.text, 'html.parser')
 rows_head = soup.findAll('table')[2]
 rows = rows_head.findAll('tr')
 times = []
-
-##### For timings
 
 for a in rows[0].findAll('td'):
     if ('AM' in a.text or 'PM' in a.text):
         times.append(a.text)
 
-#### For timings end
-days = {}
-#### For day
-days[1] = "Monday"
-days[2] = "Tuesday"
-days[3] = "Wednesday"
-days[4] = "Thursday"
-days[5] = "Friday"
-days[6] = "Saturday"
-#### For day end
-
 timetable_dict = {}
 
+
 for i in range(1, len(rows)):
-    timetable_dict[days[i]] = {}
+    timetable_dict[constants.days[i]] = {}
     tds = rows[i].findAll('td')
+    print(tds)
     time = 0
     for a in range(1, len(tds)):
-        txt = tds[a].find('b').text.strip()
+    	if tds[a].find('b'):
+        	txt = tds[a].find('b').text.strip()
+        print (txt)
         if (len(txt) >= 7):
-            timetable_dict[days[i]][times[time]] = list((tds[a].find('b').text[:7],tds[a].find('b').text[7:], int(tds[a]._attr_value_as_string('colspan'))))
-        time = time + int(tds[a]._attr_value_as_string('colspan'))
+        	if tds[a].find('b'):
+        		timetable_dict[constants.days[i]][times[time]] = list((tds[a].find('b').text[:7],tds[a].find('b').text[7:], int(tds[a]._attr_value_as_string('colspan'))))
+        	else:
+        		timetable_dict[constants.days[i]][times[time]] = list()
+        print (timetable_dict)
+
+
+    	if tds[a]._attr_value_as_string('colspan'):
+        	time = time + int(tds[a]._attr_value_as_string('colspan'))
+        else:
+        	time = time + 1
 
 
 def merge_slots(in_dict):
